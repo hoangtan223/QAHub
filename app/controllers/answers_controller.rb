55398ee9a0b213e0c55731592036aa3434d0f194
Tanbui <hoangtan223@gmail.com>
2016-02-11 13:59:44 +0700

    deploy

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,7 +3,8 @@ class AnswersController < ApplicationController
 
 		question = Question.find(params[:answer][:question_id])
 		question.answers.create(answer_params)
-
+		MainMailer.notify_question_author(question).deliver_later
+		session[:current_user_email] = answer_params[:email]
 		redirect_to question
 	end
 
